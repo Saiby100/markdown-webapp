09ef485c1ba80a7391e86f38e6921430b4a47a04
Salahuddin <Salahuddinsaiet.10@gmail.com>
2023-11-08 11:23:45 +0200

    Added react-route-dom package and defined routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
 import LoginPage from './pages/login';
 import SignupPage from './pages/signup';
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+
 
 function App() {
   return (
-    <SignupPage />
+    <Router>
+        <Routes>
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/login" element={<LoginPage />}/>  
+            <Route path="/" element={<LoginPage />}/> {/* Default path */}
+        </Routes>
+    </Router>
+
   );
 }
 
