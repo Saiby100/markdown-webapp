417c9c65a953411b0e1c3608a6d7e05defef8915
Salahuddin <Salahuddinsaiet.10@gmail.com>
2023-11-09 10:27:28 +0200

    Updated notes page

--- client/src/pages/notes.js
+++ client/src/pages/notes.js
@@ -1,10 +1,30 @@
 import React from "react";
 import "./styles/notes.scss"
 import Toolbar from "../components/toolbar";
+import { TextButton, RoundIconButton } from "../components/button";
 
 const NotePopup = ({noteTitle, noteBody}) => {
     return (
-        <div>
+        <div class="popup-bg">
+            <div class="header">
+                <input type="text" placeholder={noteTitle}/>
+
+                <div class="buttons">
+                    <div class="text-buttons">
+                        <TextButton text="Delete" plain={true} />
+                        <TextButton text="Share" plain={true} />
+                        <TextButton text="Save" />
+                    </div>
+                    <div class="icon-buttons">
+                        <RoundIconButton icon="/x.svg"/>
+                    </div>
+                </div>
+            </div>
+
+            <div class="note-area">
+                <textarea id="markdown"></textarea>
+                <div class="note-preview"></div>
+            </div>
 
         </div>
     );
@@ -24,9 +44,13 @@ const Note = ({title, body}) => {
 
 const NotesPage = () => {
 
+    const openNewNote = () => {
+
+    }
+
     return (
-        <div class="notes-background">
-            <Toolbar username="Salahuddin" />
+        <div class="background">
+            <Toolbar username="Salahuddin" newNotePress={openNewNote}/>
 
             <div class="note-list">
                 <Note 
@@ -36,6 +60,8 @@ const NotesPage = () => {
                     title="This is an even longer note title" 
                     body="This is the even longer note body"/>
             </div>
+
+            <NotePopup noteTitle="Note Title" noteBody="This is the note body"/>
         </div>
     );
 }
