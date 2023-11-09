16efea5bb96d674cabfdb2a1052004faf6ca9b64
Salahuddin <Salahuddinsaiet.10@gmail.com>
2023-11-09 11:45:57 +0200

    Added open/close functionality to notes

--- client/src/pages/notes.js
+++ client/src/pages/notes.js
@@ -4,7 +4,7 @@ import Toolbar from "../components/toolbar";
 import { TextButton, RoundIconButton } from "../components/button";
 import { marked } from "marked";
 
-const NotePopup = ({noteTitle, noteBody}) => {
+const NotePopup = ({noteTitle, noteBody, onDelete, onShare, onSave, onClose}) => {
 
     const [markdown, setMarkdown] = useState(noteBody);
     const [preview, setPreview] = useState("");
@@ -30,12 +30,12 @@ const NotePopup = ({noteTitle, noteBody}) => {
 
                 <div class="buttons">
                     <div class="text-buttons">
-                        <TextButton text="Delete" plain={true} />
-                        <TextButton text="Share" plain={true} />
-                        <TextButton text="Save" />
+                        <TextButton text="Delete" plain={true} onClick={onDelete}/>
+                        <TextButton text="Share" plain={true} onClick={onShare}/>
+                        <TextButton text="Save" onClick={onSave}/>
                     </div>
                     <div class="icon-buttons">
-                        <RoundIconButton icon="/x.svg"/>
+                        <RoundIconButton icon="/x.svg" onClick={onClose}/>
                     </div>
                 </div>
             </div>
@@ -57,13 +57,9 @@ const NotePopup = ({noteTitle, noteBody}) => {
     );
 }
 
-const Note = ({title, body}) => {
-    const handleNoteClicked = (note) => {
-        // openPopup();
-    }
-
+const Note = ({title, ...props}) => {
     return (
-        <div class="note-card" onClick={handleNoteClicked}>
+        <div class="note-card" {...props}>
             <h3>{title}</h3>
         </div>
     );
@@ -71,24 +67,69 @@ const Note = ({title, body}) => {
 
 const NotesPage = () => {
 
-    const openNewNote = () => {
+    const [noteTitle, setNoteTitle] = useState("");
+    const [noteBody, setNoteBody] = useState("");
+    const [popupVisible, setPopupVisible] = useState(false);
 
+    const openNote = (note = null) => {
+        if (note !== null) {
+            setNoteTitle(note.title);
+            setNoteBody(note.body);
+        } else {
+            setNoteTitle("Note Title");
+            setNoteBody("");
+        }
+
+        setPopupVisible(true);
     }
+
+    const closeNote = () => {
+        setPopupVisible(false);
+    }
+
+    const deleteNote = () => {
+        setPopupVisible(false);
+        //TODO: handle delete
+    }
+
+    const saveNote = () => {
+        setPopupVisible(false);
+        //TODO: handle save
+    }
+
+    const shareNote = () => {
+        alert("Share note")
+    }
+
+    const notes = [
+        {id: 1, title: "Title 1", body: "# Body 1"},
+        {id: 2, title: "Title 2", body: "# Body 2"},
+        {id: 3, title: "Title 3", body: "# Body 3"},
+        {id: 4, title: "Title 4", body: "# Body 4"}
+    ]
 
     return (
         <div class="background">
-            <Toolbar username="Salahuddin" newNotePress={openNewNote}/>
+            <Toolbar username="Salahuddin" newNotePress={openNote}/>
 
             <div class="note-list">
-                <Note 
-                    title="This is a note title" 
-                    body="This is the note body"/>
-                <Note 
-                    title="This is an even longer note title" 
-                    body="This is the even longer note body"/>
+                {
+                notes.map((note) => 
+                (<Note title={note.title} onClick={e => openNote(note)}/>))
+                }
             </div>
+            {
+                popupVisible &&
+                (<NotePopup 
+                    noteTitle={noteTitle}
+                    noteBody={noteBody}
+                    onClose={closeNote}
+                    onDelete={deleteNote}
+                    onShare={shareNote}
+                    onSave={saveNote}
+                />)
+            }
 
-            <NotePopup noteTitle="Note Title" noteBody="# This is the note body"/>
         </div>
     );
 }
