edc0b45cc6c444f356c36c3983f5b9910299ad6c
Salahuddin Saiet <Salahuddinsaiet.ss@gmail.com>
2023-12-10 15:04:00 +0200

    Added files for db models

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -1,24 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-    const User = sequelize.define('User', {
-        id: {
+    const User = sequelize.define("User", {
+        user_id: {
             type: DataTypes.INTEGER,
             primaryKey: true,
             autoIncrement: true
         },
-        username: {
-            type: DataTypes.STRING, 
-            unique: true,
-            allowNull: false
-        },
         email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true
+            }
+        },
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                notEmpty: true
+            }
+            
+        },
+        password: {
             type: DataTypes.STRING,
             allowNull: false
         }
     });
-
-    User.associate = (models) => {
-        User.hasMany(models.Note);
-    };
 
     return User;
 };
