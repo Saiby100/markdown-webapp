d85069fd0128a391d6adf03476874f6fe99988fc
Salahuddin Saiet <Salahuddinsaiet.ss@gmail.com>
2023-12-11 12:15:29 +0200

    Added new routes. Separated db mfunctions and routes

--- server/src/api.js
+++ server/src/api.js
@@ -1,17 +1,12 @@
-require('dotenv').config();
-
 const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
 const app = express();
 app.use(express.json());
 const port = 3000;
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: 'postgres',
-});
+const db = require('./database');
 
-const User = require('./models/user')(sequelize, DataTypes);
 
 app.get('/', async (req, res) => {
     res.json({message: 'Api is active'});
@@ -20,37 +15,79 @@ app.get('/', async (req, res) => {
 app.post('/add-user', async (req, res) => {
     try {
         const {name, email, password} = req.body;
-        const newUser = await User.create({
-            name: name,
-            email: email,
-            password: password
-        });
+        const newUser = await db.addUser(email, name, password);
 
         res.status(201).json({user_id: newUser.user_id, message: 'Successfully added user'});
     } catch (err)  {
-        console.log(err);
-        res.status(500).json({error: 'Internal Server Error'});
+        if (err instanceof Sequelize.UniqueConstraintError) {
+            errorMessage = err.errors[0]['message'];
+            res.status(409).json({error: errorMessage});
+
+        } else {
+            res.status(500).json({error: 'Internal Server Error'});
+        }
     }
 });
 
 app.delete('/delete-user/:userId', async (req, res) => {
     try {
-        const userId = req.params.userId;
-        const user = await User.findByPk(userId);
+        const userDeleted = db.deleteUser(req.params.userId);
 
-        if (!user) {
+        if (!userDeleted) {
             res.status(404).json({error: 'User not found'});
         }
-
-        await user.destroy();
 
         res.status(201).json({message: 'User deleted Successfully'})
 
     } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
     }
 });
 
-sequelize.sync().then(() => {
+app.post('/add-note/:userId', async (req, res) => {
+    try {
+        const {title, text} = req.body;
+        await db.addNote(req.params.userId, title, text);
+
+        res.status(201).json({message: 'Note added successfully'});
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+});
+
+app.delete('/delete-note/:noteId', async (req, res) => {
+    try {
+        const noteDeleted = db.deleteNote(req.params.noteId);
+        if (!noteDeleted) {
+            res.status(404).json({error: 'Note not found'});
+        }
+
+        res.status(201).json({message: 'Note deleted successfully'});
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+
+});
+
+app.put('/update-note/:noteId', async (req, res) => {
+    try {
+        const noteData = req.body;
+        console.log(noteData);
+        // const noteFound = db.updateNote(req.params.noteId)
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error: 'Internal Server Error'});
+    }
+
+});
+
+db.sync().then(() => {
   app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
   });
