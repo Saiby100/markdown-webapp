6cb9a5f4d8abfc798f7ecb109b0e990a81596f75
Salahuddin <Salahuddinsaiet.10@gmail.com>
2024-01-29 15:20:43 +0200

    Mounted react toastify container to app

--- client/src/App.js
+++ client/src/App.js
@@ -4,11 +4,13 @@ import RecoverPage from './pages/recover';
 import NotesPage from './pages/notes';
 import "./global.scss"
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { ToastContainer } from 'react-toastify';
 
 
 function App() {
   return (
     <Router>
+        <ToastContainer pauseOnFocusLoss={false}/>
         <Routes>
             <Route path="/signup" element={<SignupPage />} />
             <Route path="/recoverpassword" element={<RecoverPage />}/>  
